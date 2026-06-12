6b05015454a0d0973013737e9e21cdb5ac634781
Bruno Faustino <bruno.faustino5000@gmail.com>
2026-06-13 00:18:24 +0100

    Add type grammar to the output of failed invalid subtyping tests

--- freest/test/unit/SubtypingInvalidSpec.hs
+++ freest/test/unit/SubtypingInvalidSpec.hs
@@ -3,6 +3,7 @@ module SubtypingInvalidSpec (spec) where
 import Syntax.Module qualified as M
 import UI.Error ( showErrors )
 import Validation.Subtyping.Compare ( subtype )
+import Validation.Subtyping.FromTypes ( fromTypes, showGrammar )
 import UnitSpecUtils ( mkComparisonSpec )
 
 import Data.Map.Strict qualified as Map
@@ -15,4 +16,6 @@ spec :: Spec
 spec = mkComparisonSpec
   ["test/unit/SubtypingInvalid.test"]
   "Invalid subyping tests"
-  \src (t, u, k, m) -> subtype m t u `shouldBe` False
+  \src (t, u, k, m) -> case subtype m t u of
+    False -> return ()
+    True -> expectationFailure $ showGrammar $ fromTypes m [t, u]
